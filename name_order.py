031d525a9195a18ad65bc74708804852d9adb860
JunDamin <freedomgod@gmail.com>
2024-01-20 00:24:52 +0900

    add classs feature

--- name_order.py
+++ name_order.py
@@ -44,6 +44,102 @@ example = {
 }
 
 
+class Item(Draggable):
+    def __init__(
+        self,
+        page,
+        group,
+        text,
+        parent=None,
+        drag_will_accept=None,
+        drag_leave=None,
+        width=200,
+        height=50,
+        bgcolor=colors.GREY_100,
+        border_radius=10,
+        alignment=ft.alignment.center,
+    ):
+        self.page = page
+        self._content_text = text
+        self.parent = parent
+        self._color = bgcolor
+        self.content = DragTarget(
+            self,
+            group=group,
+            content=Container(
+                content=ft.Text(text),
+                width=width,
+                height=height,
+                bgcolor=self.color,
+                border_radius=border_radius,
+                alignment=alignment,
+            ),
+            on_will_accept=drag_will_accept,
+            on_accept=self.drag_accept,
+            on_leave=drag_leave,
+        )
+        self.content.parent = self
+        super().__init__(content=self.content, group=group)
+
+    @property
+    def content_text(self):
+        return self._content_text
+
+    @content_text.setter
+    def content_text(self, text):
+        self._content_text = text
+        self.content.content.content = ft.Text(text)
+
+    @property
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, bgcolor):
+        self._color = bgcolor
+        self.content.content.bgcolor = bgcolor
+
+    def drag_accept(self, e: DragTargetAcceptEvent):
+        e.control.content.border = None
+        src = self.page.get_control(e.src_id)
+        target = e.control.parent
+
+        src.color, target.color = target.color, src.color
+        src.content_text, target.content_text = target.content_text, src.content_text
+
+        src.update()
+        target.update()
+
+        if self.parent:
+            self.parent.update()
+
+
+class Fields:
+    def __init__(self):
+        self._items = []
+    
+    @property
+    def items(self):
+        return self._items
+    
+    @items.setter
+    def items(self, items):
+        self._items = items
+        self.order = [item.content_text for item in self._items]
+        self.filename = "_".join(
+            [example.get(text) for text in self.order if text]
+        )
+        self.filename_display = ft.Text(self.filename)
+
+    def update(self):
+        self.order[:] = [item.content_text for item in self.items]
+        self.filename = "_".join(
+            [example.get(text) for text in self.order if text]
+        )
+        self.filename_display.value = self.filename
+        self.filename_display.update()
+
+
 def nameController(page):
     def drag_will_accept(e):
         e.control.content.border = border.all(
@@ -51,74 +147,34 @@ def nameController(page):
         )
         e.control.update()
 
-    def drag_accept(e: DragTargetAcceptEvent):
-        src = page.get_control(e.src_id)
-        e.control.content.bgcolor = src.content.bgcolor
-        e.control.content.border = None
-        # set text
-        e.control.content.content = ft.Text(src.content.content.value)
-        e.control.update()
-        filename_display.value = "_".join(
-            [
-                example.get(control.content.content.value)
-                for control in filename_field.controls
-                if control.content.content and control.content.content.value
-            ]
-        )
-        filename_display.update()
-
-        # update attribute
-        draggable_name_contorl.name_order = [
-            control.content.content.value
-            for control in filename_field.controls
-            if control.content.content
-        ]
-
     def drag_leave(e):
         e.control.content.border = None
         e.control.update()
 
-    filename_field = Row(
-        [
-            DragTarget(
+    fields = Fields()
+    fields.items = [
+            Item(
+                page=page,
                 group="eml",
-                content=Container(**get_setting(name, bgcolor)),
-                on_will_accept=drag_will_accept,
-                on_accept=drag_accept,
-                on_leave=drag_leave,
+                text="",
+                drag_will_accept=drag_will_accept,
+                drag_leave=drag_leave,
+                parent=fields
             )
-            for name, bgcolor in initial_setting.items()
+            for _ in range(4)
         ]
-    )
-
-    filename_display = ft.Text(
-        "_".join(
-            [
-                example.get(control.content.content.value)
-                for control in filename_field.controls
-                if control.content.content and control.content.content.value
-            ]
-        )
-    )
+    
+    print(fields.items)
+    
 
     option_list = [
-        Draggable(
+        Item(
+            page,
             group="eml",
-            content=Container(**get_setting(name, bgcolor)),
+            text=name,
+            bgcolor=bgcolor,
         )
         for name, bgcolor in options_setting.items()
-    ] + [
-        Draggable(
-            group="eml",
-            content=Container(
-                content=ft.Text(""),
-                width=200,
-                height=50,
-                bgcolor=colors.BLUE_GREY_100,
-                border_radius=5,
-                alignment=ft.alignment.center,
-            ),
-        ),
     ]
 
     draggable_name_contorl = Column(
@@ -141,8 +197,7 @@ def nameController(page):
                         ft.Text(
                             "파일명 형식: ", theme_style=ft.TextThemeStyle.HEADLINE_SMALL
                         ),
-                        filename_field,
-                    ],
+                     *fields.items],
                     alignment=ft.MainAxisAlignment.CENTER,
                 ),
                 bgcolor=colors.BLUE_100,
@@ -153,14 +208,14 @@ def nameController(page):
                 [
                     ft.Container(width=50),
                     ft.Text("파일명 예시: ", theme_style=ft.TextThemeStyle.BODY_MEDIUM),
-                    filename_display,
+                    fields.filename_display,
                 ],
                 alignment=ft.MainAxisAlignment.CENTER,
             ),
         ]
     )
 
-    draggable_name_contorl.name_order = [*initial_setting.keys()]
+    draggable_name_contorl.name_order = fields.order
 
     return draggable_name_contorl
 
